d586dbff89708c42c88b9bf75f0e4dfee74d7f47
wruttan <wruttan@sterlingcrane.com>
2021-09-27 17:58:13 -0600

    updated script so that it will update the CSV if it can update the user or not

--- UserUpdate.ps1
+++ UserUpdate.ps1
@@ -1,44 +1,85 @@
 <#
 	.SYNOPSIS
-	This script will create the Test Zones, DNS Entries and then delete and add the new DNS Entries just like the real script
+	This script will update the Country Code for users in Active Directory
 	.DESCRIPTION
-	This script will do the following
-        - Create 4 test zones
-        - Create the DNS entries in the test zones
-        - Test to make sure that the entries are created
-        - Cleans up the zones after the script is done
+	This script does the following:
+        - Determines if the user account exists
+        - Determines if the user account is enabled
+        - Determines if the user account has a country set or not
+        - If the country is not set then the script sets the country
 	.OUTPUTS
-	Will output a display of the DNS records after they are created and then email the script user the results of the test
+	When run the script will display the user accounts that have been updated.  The script will also update the CSV to say if the account was updated or not
 	
 	.EXAMPLE
-	
+	.\UserUpdate.ps1
+
 	.NOTES
-		ScriptName	: TestDNSChanges.ps1
+		ScriptName	: UserUpdate.ps1
 		Created by  : Winston Ruttan
 		Date Coded  : 09/09/2021
 #>
 
-
+#Imports the CSV file
 $Users = Import-Csv -path ".\Lib\UserUpdate1.csv" -Header "LegalName","Username","Country","Complete" | Select-Object -Skip 1
 
-foreach ($u in $Users)
-{
-    try {
-        $ADUser = Get-ADUser -Identity $u.Username -ErrorAction Stop
-    }
-    catch {
-        if($_ -like "*Cannot find an object with identity: '$u.Username'*"){
-            $u.Username + "Doesn't exist"
-        }
-        continue
-    }
-    
-    $Enabled = Get-ADUser -Identity $u.Username -Properties * 
+#Imports the CSV file again, possibily redenudant I will have to look into that
+$table = Import-Csv -Path .\lib\UserUpdate1.csv 
 
-    if($Enabled.Enabled -eq $True){
-        if($Enabled.Country -eq $null){
-            $u.Username + " will be updated"
-            set-aduser -Identity $u.Username -Country "CA"
+#Looks at the loaded $Users variable and for each of them does the following
+
+function UpdateCountry {
+    foreach ($u in $Users)
+    {
+        #Tries to make sure that the user account exists
+        try {
+            #Tries to load the user infor from AD into a varialbe
+            $ADUser = Get-ADUser -Identity $u.Username -Properties * -ErrorAction Stop
         }
+        catch {
+            #If it's unable to load the user info it updates the CSV with "Completed" type NA
+            $RowIndex = [array]::IndexOf($table.Username,$u.Username)
+            if($RowIndex -gt -1)
+            {
+                if($table[$RowIndex].Complete -eq ""){
+                    $table[$RowIndex].Complete = "NA"
+                    $table | Export-Csv .\Lib\UserUpdate1.csv -NoTypeInformation 
+                }
+
+            continue
+            }
+        }
+        
+        #Loads AD User info into a variable
+        $Enabled = Get-ADUser -Identity $u.Username -Properties *
+        
+        #Load the Row index
+        $RowIndex = [array]::IndexOf($table.Username,$u.Username)
+        
+        if($Enabled.Enabled -eq $True -and $Enabled.Country -eq $null){
+            $u.Username + " will be updated"
+
+            #set-aduser -Identity $u.Username -Country "CA"
+
+            UpdateCSV
+
+        }else{
+            UpdateCSV
+        }  
     }
 }
+
+
+
+function UpdateCSV {
+
+    if($table[$RowIndex].Complete -eq ""){
+        $table[$RowIndex].Complete = "Yes"
+        $table | Export-Csv .\Lib\UserUpdate1.csv -NoTypeInformation 
+    }else{
+        $table[$RowIndex].Complete = "Yes"
+        $table | Export-Csv .\Lib\UserUpdate1.csv -NoTypeInformation
+    }
+    
+}
+
+UpdateCountry
