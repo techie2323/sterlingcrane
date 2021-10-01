f2808e5f7dc2dad8e920effcb4940bc497964ddf
wruttan <wruttan@sterlingcrane.com>
2021-10-01 10:29:44 -0600

    added new files

--- AddEmail.ps1
+++ AddEmail.ps1
@@ -1,22 +1,7 @@
 Add-Type -AssemblyName PresentationFramework
 
-Function Get-FixedDisk {
-    [CmdletBinding()]
-    # This param() block indicates the start of parameters declaration
-    param (
-        <# 
-            This parameter accepts the name of the target computer.
-            It is also set to mandatory so that the function does not execute without specifying the value.
-        #>
-        [Parameter(Mandatory)]
-        [string]$Computer
-    )
-    <#
-        WMI query command which gets the list of all logical disks and saves the results to a variable named $DiskInfo
-    #>
-    $DiskInfo = Get-WmiObject Win32_LogicalDisk -ComputerName $Computer -Filter 'DriveType=3'
-    $DiskInfo
-}
+
+
 
 # where is the XAML file?
 $xamlFile = "MainWindow.xaml"
